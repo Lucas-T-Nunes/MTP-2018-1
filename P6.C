23ddaa5545fdbef19e536cc0f6745c0b66c75457
Lucas-T-Nunes <38742484+Lucas-T-Nunes@users.noreply.github.com>
2018-06-20 23:29:51 -0300

    Add files via upload

---
+++ P6.C
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int som(int num[], int a)
+{
+	int soma = 0, i;
+	for(i = 0; i < a; i++)
+	{
+		soma = soma + num[i];
+	}
+	return soma;
+}
+float divisao(float soma, float a)
+{
+	float media;
+	media = soma / a;
+	return media;
+}
+int main()
+{
+	int a, j, num[20], resultado;
+	printf("\nDigite a quantidade de valores que ira digitar(entre 5 e 20): ");
+	scanf("%d", &a);
+	getchar();
+	if(a >= 5 && a <= 20)
+	{
+	    for(j = 0; j < a; j++)
+	    {	
+		    printf("Valor %d: ", j+1);
+		    scanf("%d", &num[j]);
+		    getchar();
+	    }
+    }
+    else
+    {
+    	printf("Numero digitado invalido. ");
+	}
+	while(a >= 5 && a <= 20)
+	{
+	    resultado = som(num, a);
+	    printf("\nMedia = %.2f", divisao(resultado, a));
+	    return 0;
+    }
+}
